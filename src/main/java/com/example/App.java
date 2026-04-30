30dfef861b153bd5ed5509aa968161d7f0bb1bfa
skossel <143590242+skossel@users.noreply.github.com>
2026-04-30 15:38:07 +0200

    feat: Java/Gradle Projektstruktur aufsetzen (closes #1)

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(getGreeting());
+    }
+
+    public static String getGreeting() {
+        return "Hello, World!";
+    }
+}
